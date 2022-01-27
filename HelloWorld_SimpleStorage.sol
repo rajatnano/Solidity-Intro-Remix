e6c2a63259b367769032006993004729a4416fe6
Rajat <98547181+rajatnano@users.noreply.github.com>
2022-01-27 23:09:51 +0530

    Create HelloWorld_SimpleStorage.sol
    the following code is "Hello World" program for Solidity

---
+++ HelloWorld_SimpleStorage.sol
@@ -0,0 +1,23 @@
+//the following code is "Hello World" program for Solidity 
+
+pragma solidity ^0.6.0;
+//runs in any version above 0.6.0 but below 0.7.0
+
+
+//name and initiate a contract
+contract SimpleStorage{  
+    
+    //declare variable; default value will be take as 0; not public by default
+    uint256  public favoriteNumber;  
+
+    //declare a public function and parameter
+    function store(uint256 _favotiteNumber) public { 
+        //function operation; store double of input number
+        favoriteNumber = 2*_favotiteNumber;
+    }
+}
+
+/*deployed contract
+Orange (store) --> not free operation a.k.a transaction on blockchain; cost gas
+Blue (favoriteNumber) --> view data from blockchain i.e. does cost gas
+*/
