46ebbf4f2ab152b41dea0b7cfb7f682941b463ed
Rajat <98547181+rajatnano@users.noreply.github.com>
2022-01-29 20:59:26 +0530

    Create SimpleStorage3.sol
    store list of people with their name and favorite number in a list.
    search content of list using index value.
    search favorite number using name with the help of mapping functionality.

---
+++ SimpleStorage3.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+/*Program to store list of people with their name and favorite number
+with added functionality of search a person's favortie number using their name*/
+
+pragma solidity ^0.8.0;
+
+contract SimpleStorageList {
+
+    //initiate a structure "People" to store name and favortie number
+    struct People {
+        string name;
+        uint256 favoriteNumber;
+    }
+
+    //People[] creates dynamic array; People[10] will create a static array with max 10 entries 
+    People[] public people;
+
+    //to search favoriteNumber using people's name
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People({name: _name, favoriteNumber: _favoriteNumber}));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    /*string in solidity is an array object and not a datatype 
+    hence we can store it's contents in memory or storage 
+    ..memory stores only for/while execution
+    ..storage stores forever*/
+}
